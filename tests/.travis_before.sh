82203821070d9ca11de02159aa3b61fd95bd952e
Dionysios Fryganas <dfryganas@gmail.com>
2014-11-20 02:17:58 +0100

    Remove tests folder and use wp-cli tool instead.
    This allows more flexibility during development.
    
    * [Blog post on benefits](http://wp.mattwie.be/2014/01/15/why-you-should-develop-plugins-with-wp-cli/#fn-431-1)
    * [Docs on UnitTesting](https://github.com/wp-cli/wp-cli/wiki/Plugin-Unit-Tests)
    
    Future commits will include woocommerce installation for testing
    and {back,front}-end test cases.

--- tests/.travis_before.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/tierra/wordpress.git /tmp/wordpress
-git clone . "/tmp/wordpress/src/wp-content/plugins/$PLUGIN_SLUG"
-cd /tmp/wordpress
-git checkout $WP_VERSION
-mysql -e "CREATE DATABASE wordpress_tests;" -uroot
-cp wp-tests-config-sample.php wp-tests-config.php
-sed -i "s/youremptytestdbnamehere/wordpress_tests/" wp-tests-config.php
-sed -i "s/yourusernamehere/travis/" wp-tests-config.php
-sed -i "s/yourpasswordhere//" wp-tests-config.php
