e2af96a5c28f6502f257c4f5920a55098fd6805d
Dionysios Fryganas <dfryganas@gmail.com>
2014-11-21 05:33:41 +0100

    First tests are finally shipping!

--- tests/test-pro6pp.php
+++ tests/test-pro6pp.php
@@ -1,8 +1,36 @@
 <?php
-require_once (dirname(__DIR__) . '/pro6pp.php');
+require_once (dirname(
+        __DIR__) . '/pro6pp.php');
 
-class WP_Test_Pro6pp_Class extends WP_UnitTestCase
+class Pro6ppTest extends WP_UnitTestCase
 {
+
+    /**
+     * Fancy function to call private members of classes.
+     * Uses reflection
+     *
+     * @uses php >= 5.3.4
+     * @see http://stackoverflow.com/questions/5937845/mock-private-method-with-phpunit
+     * @param Class $object
+     * @param Function $methodName
+     */
+    public function callPrivateMethod ($object, $methodName)
+    {
+        $reflectionClass = new \ReflectionClass($object);
+        $reflectionMethod = $reflectionClass->getMethod(
+                $methodName);
+        $reflectionMethod->setAccessible(
+                true);
+
+        $params = array_slice(
+                func_get_args(),
+                2); // get all the
+                                                           // parameters after
+                                                           // $methodName
+        return $reflectionMethod->invokeArgs(
+                $object,
+                $params);
+    }
 
     function setUp ()
     {
@@ -11,20 +39,70 @@ class WP_Test_Pro6pp_Class extends WP_UnitTestCase
         // Suppress warnings from "Cannot modify header information - headers
         // already sent by"
         $this->_error_level = error_reporting();
-        error_reporting($this->_error_level & ~ E_WARNING);
+        error_reporting(
+                $this->_error_level & ~ E_WARNING);
     }
 
     function testPro6pp ()
     {
         // Assume success.
-        $this->assertTrue(true);
+        /*   $this->assertTrue(
+                true); */
     }
 
-    function testWoocommerceInstalled ()
+    /**
+     * @expectedException WPDieException
+     */
+    function testInvalidRefererWithValidateReferer ()
     {
-        $this->markTestSkipped(
-                'pro6pp tests are pending creation.'
+        $_SERVER['HTTP_REFERER'] = 'Possible_jiberish_example_from_another_domain..';
+        $this->assertNotEmpty(
+                $this->callPrivateMethod(
+                        $this->pro6pp,
+                        'validate_referer'));
+    }
+
+    function testValidRefererWithValidateReferer ()
+    {
+        $_SERVER['HTTP_REFERER'] = site_url() . '?foo=bar&bar=foo';
+        $this->assertEmpty(
+                $this->callPrivateMethod(
+                        $this->pro6pp,
+                        'validate_referer'));
+        $_SERVER['HTTP_REFERER'] = admin_url() . '/some/stuff/here';
+        $this->assertEmpty(
+                $this->callPrivateMethod(
+                        $this->pro6pp,
+                        'validate_referer'));
+    }
+
+    function testSettingsLink ()
+    {
+        $links = array(
+                'dummy' => 'string'
         );
+        $adminUrl = admin_url();
+        $expected = array(
+                'dummy' => 'string',
+                'Settings' => '<a href="' . $adminUrl .
+                         'admin.php?page=pro6pp_autocomplete">Settings</a>'
+        );
+        $this->assertArrayHasKey(
+                'Settings',
+                $this->pro6pp->pro6pp_settings_link(
+                        $links));
+        $this->assertArrayHasKey(
+                'dummy',
+                $this->pro6pp->pro6pp_settings_link(
+                        $links));
+        $this->assertEqualSets(
+                $expected,
+                $this->pro6pp->pro6pp_settings_link(
+                        $links));
+    }
+
+    function testClientScripts ()
+    {
     }
 }
 
