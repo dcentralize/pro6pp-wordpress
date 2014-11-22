0169342f7d58b09c076b0660945f3fc5d5bb7f42
Dionysios Fryganas <dfryganas@gmail.com>
2014-11-22 02:37:36 +0100

    Added more tests for the Pro6pp class.

--- tests/test-pro6pp.php
+++ tests/test-pro6pp.php
@@ -1,6 +1,5 @@
 <?php
-require_once (dirname(
-        __DIR__) . '/pro6pp.php');
+require_once (dirname(__DIR__) . '/pro6pp.php');
 
 class Pro6ppTest extends WP_UnitTestCase
 {
@@ -17,19 +16,13 @@ class Pro6ppTest extends WP_UnitTestCase
     public function callPrivateMethod ($object, $methodName)
     {
         $reflectionClass = new \ReflectionClass($object);
-        $reflectionMethod = $reflectionClass->getMethod(
-                $methodName);
-        $reflectionMethod->setAccessible(
-                true);
+        $reflectionMethod = $reflectionClass->getMethod($methodName);
+        $reflectionMethod->setAccessible(true);
 
-        $params = array_slice(
-                func_get_args(),
-                2); // get all the
-                                                           // parameters after
-                                                           // $methodName
-        return $reflectionMethod->invokeArgs(
-                $object,
-                $params);
+        $params = array_slice(func_get_args(), 2); // get all the
+                                                   // parameters after
+                                                   // $methodName
+        return $reflectionMethod->invokeArgs($object, $params);
     }
 
     function setUp ()
@@ -39,15 +32,122 @@ class Pro6ppTest extends WP_UnitTestCase
         // Suppress warnings from "Cannot modify header information - headers
         // already sent by"
         $this->_error_level = error_reporting();
-        error_reporting(
-                $this->_error_level & ~ E_WARNING);
+        error_reporting($this->_error_level & ~ E_WARNING);
     }
 
-    function testPro6pp ()
+    function testSettingsLink ()
     {
-        // Assume success.
-        /*   $this->assertTrue(
-                true); */
+        $links = array(
+                'dummy' => 'string'
+        );
+        $expected = array(
+                'dummy' => 'string',
+                'Settings' => '<a href="' . admin_url() .
+                         'admin.php?page=pro6pp_autocomplete">Settings</a>'
+        );
+        $this->assertArrayHasKey('Settings',
+                $this->pro6pp->pro6pp_settings_link($links));
+        $this->assertArrayHasKey('dummy',
+                $this->pro6pp->pro6pp_settings_link($links));
+
+        $this->assertEqualSets($expected,
+                $this->pro6pp->pro6pp_settings_link($links));
+    }
+
+    function testWoocommerceStates ()
+    {
+        // Dummy values with their equivelant states.
+        $countries = $expected = array(
+                'GB' => array(
+                        'dummy',
+                        'value'
+                ),
+                'NL' => array(
+                        'foo' => 'bar'
+                ),
+                'BE' => array(
+                        'bar' => 'foo'
+                ),
+                'US' => array(
+                        'dummy' => 'data'
+                )
+        );
+        $actual = $this->pro6pp->pro6pp_woocommerce_states($countries);
+
+        $this->assertArrayHasKey('GB', $actual);
+        $this->assertArrayHasKey('NL', $actual);
+        $this->assertArrayHasKey('BE', $actual);
+        $this->assertArrayHasKey('bar', $actual['BE']);
+        $this->assertArrayHasKey('US', $actual);
+
+        $this->assertTrue(is_array($actual['NL']));
+
+        $this->assertArrayNotHasKey('foo', $actual['NL']);
+
+        $this->assertEquals(12, count($actual['NL']));
+    }
+
+    function testOverrideAddressFields ()
+    {
+        $address_fields = $expected = array(
+                'dummy' => array(
+                        'foo' => 'bar'
+                ),
+                'address_2' => array(
+                        'label' => 'LABEL',
+                        'class' => 'CSS_CLASS'
+                )
+        );
+        $expectedAdress2Class = array(
+                'form-row',
+                'form-row-wide'
+        );
+
+        $actual = $this->pro6pp->pro6pp_override_default_address_fields(
+                $address_fields);
+
+        $this->assertEquals('Streetnumber', $actual['address_2']['label']);
+        $this->assertTrue(is_array($actual['address_2']['class']));
+        $this->assertEquals($expectedAdress2Class,
+                $actual['address_2']['class']);
+    }
+
+    function test_Empty_OverrideAddressFields ()
+    {
+        $address_fields = $expected = array(
+                'dummy' => array(
+                        'foo' => 'bar'
+                ),
+                'dumm-dummy' => array(
+                        'foo' => 'bar',
+                        'bar' => 'foo'
+                )
+        );
+        $actual = $this->pro6pp->pro6pp_override_default_address_fields(
+                $address_fields);
+
+        // We set the key explicitly if not present. Is that OK or refactor?
+        $this->arrayHasKey('address_2', $actual);
+    }
+
+    function testOverrideDefaultCheckoutFields ()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+
+    function testAddressValidation ()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+
+    function testHandleRequest ()
+    {
+        $this->markTestSkipped('Not implemented.');
+    }
+
+    function testErrorOccured ()
+    {
+        $this->markTestSkipped('Not implemented.');
     }
 
     /**
@@ -57,52 +157,16 @@ class Pro6ppTest extends WP_UnitTestCase
     {
         $_SERVER['HTTP_REFERER'] = 'Possible_jiberish_example_from_another_domain..';
         $this->assertNotEmpty(
-                $this->callPrivateMethod(
-                        $this->pro6pp,
-                        'validate_referer'));
+                $this->callPrivateMethod($this->pro6pp, 'validate_referer'));
     }
 
     function testValidRefererWithValidateReferer ()
     {
         $_SERVER['HTTP_REFERER'] = site_url() . '?foo=bar&bar=foo';
         $this->assertEmpty(
-                $this->callPrivateMethod(
-                        $this->pro6pp,
-                        'validate_referer'));
+                $this->callPrivateMethod($this->pro6pp, 'validate_referer'));
         $_SERVER['HTTP_REFERER'] = admin_url() . '/some/stuff/here';
         $this->assertEmpty(
-                $this->callPrivateMethod(
-                        $this->pro6pp,
-                        'validate_referer'));
-    }
-
-    function testSettingsLink ()
-    {
-        $links = array(
-                'dummy' => 'string'
-        );
-        $adminUrl = admin_url();
-        $expected = array(
-                'dummy' => 'string',
-                'Settings' => '<a href="' . $adminUrl .
-                         'admin.php?page=pro6pp_autocomplete">Settings</a>'
-        );
-        $this->assertArrayHasKey(
-                'Settings',
-                $this->pro6pp->pro6pp_settings_link(
-                        $links));
-        $this->assertArrayHasKey(
-                'dummy',
-                $this->pro6pp->pro6pp_settings_link(
-                        $links));
-        $this->assertEqualSets(
-                $expected,
-                $this->pro6pp->pro6pp_settings_link(
-                        $links));
-    }
-
-    function testClientScripts ()
-    {
+                $this->callPrivateMethod($this->pro6pp, 'validate_referer'));
     }
 }
-
